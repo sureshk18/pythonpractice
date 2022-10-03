70e6b26cb5bf6fde8a3f06f5f5489373b36cfb73
Suresh Choudhary <mesuresh318@gmail.com>
2022-10-04 00:11:49 +0530

    PYTHON PRACTICE

---
+++ var.py
@@ -0,0 +1,21 @@
+from traceback import print_list
+
+
+x = 100
+print(x)
+print(type(x))
+
+n = 12.4
+print(type(n))
+
+a = "suresh choudhary"
+print(a)
+print(type(a))
+
+list = [1, 2, 3, 4, 5]
+print(list)
+print(list[3])
+
+# update value
+list[4] = 12
+print(list)
