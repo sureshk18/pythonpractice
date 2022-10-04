011f858ea4c036b7436ced8f766e6f3fb882cb2a
Suresh Choudhary <mesuresh318@gmail.com>
2022-10-04 21:32:30 +0530

    python practice

--- var.py
+++ var.py
@@ -1,3 +1,4 @@
+
 x = 100
 print(x)
 print(type(x))
@@ -31,3 +32,46 @@ a = 10
 b = 3
 print(a//b)
 print(a % b)
+
+# string operator
+s = "practice makes perfect"
+print(s[0:11])
+print(s[:4])
+print(s[:30])
+
+
+# python numbers
+num = 456
+print(type(num))
+x = 2+5j
+print(type(x))
+
+X = 2.5
+print(X)
+print(type(X))
+
+# string
+var = "simpli-learn"
+print(var[0:5])
+
+
+# python list
+
+list = [1, 2, 3, "suresh kumar", 4, 5, 6]
+# index(0,  1, 2,   3,     4,     5, 6, 7)
+print(list)
+print(list[3])
+
+s = [1, 2, 3, 3]
+print(s)  # int
+s = ['a', 'b', 'c', 'd']
+print(s)  # letter
+s = ["hy", "suresh"]
+print(s)  # string
+
+
+mix = [1, 3, "sureshkumar"]
+print(mix[0:4])
+
+mat = [[1, 2, 3], ['a', 'b']]
+print(mat)
